7158b0ed562ec124cd31cb5a9784568cca6cfaa9
hmlu36 <hmlu36@gmail.com>
2021-05-19 15:25:56 +0800

    'deploy'

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/WeddingForm/', //發佈對應路徑
+  base: '/vite-bible/', //發佈對應路徑
   assetsDir: 'assets',
 })
