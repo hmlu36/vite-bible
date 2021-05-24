7de56e4d01c1b0bff951b6f0dc45fa6e64ce9f9e
hmlu36 <hmlu36@gmail.com>
2021-05-24 11:30:50 +0800

    'deploy'

--- src/App.vue
+++ src/App.vue
@@ -26,18 +26,28 @@ export default {
 };
 </script>
 <style>
-/*@import url(https://fonts.googleapis.com/earlyaccess/cwtexfangsong.css);*/
+@import url(https://fonts.googleapis.com/earlyaccess/cwtexfangsong.css);
 /*@import url(https://fonts.googleapis.com/earlyaccess/cwtexming.css);
 /*@import url(https://fonts.googleapis.com/earlyaccess/cwtexyen.css);*/
 /*@import url(https://fonts.googleapis.com/earlyaccess/notosanstc.css);*/
 
-*,
-::placeholder {
-  /*font-family: "cwTeXFangSong", sans-serif;*/
-  font-size: 20px;
-  /*font-family: 'cwTeXMing', sans-serif;*/
-  /*font-family: 'cwTeXYen', sans-serif;*/
-  /*font-family: 'Noto Sans TC', sans-serif;*/
+@media (min-width: 1025px) {
+  *,
+  ::placeholder {
+    font-family: "cwTeXFangSong", sans-serif;
+    font-size: 20px;
+    /*font-family: 'cwTeXMing', sans-serif;*/
+    /*font-family: 'cwTeXYen', sans-serif;*/
+    /*font-family: 'Noto Sans TC', sans-serif;*/
+  }
+}
+
+@media (max-width: 1025px) {
+  /* CSS */
+  *,
+  ::placeholder {
+    font-size: 20px;
+  }
 }
 
 .text-right {
